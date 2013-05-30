d1859af5264864f0b0624d386205175297cae77f
Eric Mooney <eric.c.mooney@gmail.com>
2013-05-30 16:47:21 -0700

    Add client class

---
+++ client.rb
@@ -0,0 +1,9 @@
+class Client
+  attr_accessor :name, :pets
+
+  def initialize(name)
+    @name = name
+    @pets = []
+  end
+
+end
--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require "pry"
 
 require_relative "shelter"
+require_relative "client"
 
 shelter = Shelter.new
 
@@ -10,8 +11,18 @@ response = gets.chomp.downcase
 while response != "q"
   if response == "l"
 
-    puts shelter.pets.each
-    
+    shelter.pets.each do |pet|
+      puts pet
+      end
+  elsif response == "a"
+    puts "Which pet would you like?"
+    pet_to_adopt = gets.chomp.capitalize
+    shelter.pets.delete(pet_to_adopt)
+
+    puts "What is your first name?"
+    client_name = gets.chomp.capitalize
+    clients = Client.new(client_name)
+        
   end
   print "Do you want to (l)ist available pets, (a)dopt a pet, (g)ive up a pet, or (q)uit?"
   response = gets.chomp.downcase
